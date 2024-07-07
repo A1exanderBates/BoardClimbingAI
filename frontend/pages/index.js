07a4e3a3030e320e3cb4215faae040232724a599
A1exanderBates <alexanderbates.edu@gmail.com>
2024-07-07 09:10:08 -0400

    Initial commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Board Climb AI Web App</h1>
+        <p>Welcome to your new climbing analysis application!</p>
+      </div>
+    )
+  }
